32335c4a31aa2f3ed89da186d069d73c34f565f1
Filipe Roberto <filipe-domingos@hotmail.com>
2016-05-03 15:52:25 -0300

    migração Excluídos

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :horarios
-  resources :horarios
-  resources :horarios
-  resources :horarios
-  resources :horarios
   resources :linhas
   resources :onibus
   resources :pontos
